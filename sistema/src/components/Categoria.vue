e621cd2ac94bceef6b06d1c2c39fbfeeb4d249a8
Irving Herrera <irving.herreramolina@gmail.com>
2019-01-15 17:05:47 -0600

    funcionalidad para crear categoria

--- sistema/src/components/Categoria.vue
+++ sistema/src/components/Categoria.vue
@@ -13,7 +13,7 @@
         <v-text-field class="text-xs-center" v-model="search" append-icon="search" label="Búsqueda" single-line hide-details></v-text-field>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
-          <v-btn slot="activator" color="primary" dark class="mb-2">New Item</v-btn>
+          <v-btn slot="activator" color="primary" dark class="mb-2">Nuevo</v-btn>
           <v-card>
             <v-card-title>
               <span class="headline">{{ formTitle }}</span>
@@ -22,20 +22,11 @@
             <v-card-text>
               <v-container grid-list-md>
                 <v-layout wrap>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                  <v-flex xs12 sm12 md12>
+                    <v-text-field v-model="nombre" label="Nombre"></v-text-field>
                   </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                  <v-flex xs12 sm12 md12>
+                    <v-text-field v-model="descripcion" label="Descripción"></v-text-field>
                   </v-flex>
                 </v-layout>
               </v-container>
@@ -43,8 +34,8 @@
   
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-              <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+              <v-btn color="blue darken-1" flat @click="close">Cancelar</v-btn>
+              <v-btn color="blue darken-1" flat @click="guardar">Guardar</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -108,24 +99,20 @@ export default {
             search: '',
             editedIndex: -1,
             editedItem: {
-            name: '',
-            calories: 0,
-            fat: 0,
-            carbs: 0,
-            protein: 0
+                name: '',
+                calories: 0,
+                fat: 0,
+                carbs: 0,
+                protein: 0
             },
-            defaultItem: {
-            name: '',
-            calories: 0,
-            fat: 0,
-            carbs: 0,
-            protein: 0
-            }
+            id: '',
+            nombre: '',
+            descripcion: ''
         }
     },
         computed: {
         formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        return this.editedIndex === -1 ? 'Nueva categoría' : 'Actualizando categoria'
         }
     },
 
@@ -151,31 +138,46 @@ export default {
             }) 
         }, 
             editItem (item) {
-            this.editedIndex = this.desserts.indexOf(item)
-            this.editedItem = Object.assign({}, item)
-            this.dialog = true
+                this.editedIndex = this.desserts.indexOf(item)
+                this.editedItem = Object.assign({}, item)
+                this.dialog = true
             },
 
             deleteItem (item) {
-            const index = this.desserts.indexOf(item)
-            confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+                const index = this.desserts.indexOf(item)
+                confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
             },
 
             close () {
-            this.dialog = false
-            setTimeout(() => {
-                this.editedItem = Object.assign({}, this.defaultItem)
-                this.editedIndex = -1
-            }, 300)
+                this.dialog = false;
             },
 
-            save () {
-            if (this.editedIndex > -1) {
-                Object.assign(this.desserts[this.editedIndex], this.editedItem)
-            } else {
-                this.desserts.push(this.editedItem)
-            }
-            this.close()
+            limpiar() {
+                this.id = '';
+                this.nombre = '';
+                this.deleteItem =  '';
+            },
+
+            guardar () {
+                if (this.editedIndex > -1) {
+                    // codigo para editar
+                } else {
+                    // codigo para guardar
+                    let me = this;
+                    axios.post('api/Categorias/Crear', { 
+                        'nombre': me.nombre,
+                        'descripcion': me.descripcion
+                     })
+                    .then(function (response) {
+                        console.log(response);
+                        me.close();
+                        me.listar();
+                        me.limpiar();
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                }
             }
     }
 }
