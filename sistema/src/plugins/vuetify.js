d32ba9bd668eb5fe9dde8571eafb2c2e5cd27b0f
Irving Herrera <irving.herreramolina@gmail.com>
2019-01-15 17:10:41 -0600

    cambio de idioma a español

--- sistema/src/plugins/vuetify.js
+++ sistema/src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import es from 'vuetify/es5/locale/es'
 import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
@@ -14,4 +15,8 @@ Vue.use(Vuetify, {
     },
     customProperties: true,
     iconfont: 'md',
+    lang: {
+        locales: { es },
+        current: 'es'
+    }
 })
