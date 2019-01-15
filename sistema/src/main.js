cdd498ab13a15814ed679ae7aa45ae673d9995db
Irving Herrera <irving.herreramolina@gmail.com>
2019-01-15 16:33:45 -0600

    se implemento axios para obtener el listado de las categorias

--- sistema/src/main.js
+++ sistema/src/main.js
@@ -3,9 +3,11 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = 'http://localhost:49290/';
 new Vue({
     router,
     store,
