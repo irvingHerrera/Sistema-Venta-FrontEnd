87c8e4ef9614194c62e691a18316ef80d8c94aca
Irving Herrera <irving.herreramolina@gmail.com>
2019-01-17 11:14:20 -0600

    se implemento componente de roles

--- sistema/src/router.js
+++ sistema/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Categoria from './components/Categoria.vue';
 import Articulo from './components/Articulo.vue';
+import Rol from './components/Rol.vue';
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
             path: '/articulos',
             name: 'articulos',
             component: Articulo
+        },
+        {
+            path: '/roles',
+            name: 'rol',
+            component: Rol
         },
     ]
 })
